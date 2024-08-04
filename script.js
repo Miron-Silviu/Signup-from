d7fb5f20cacd79e8e8fbfaaf7a81eea72fb7c5ad
Silviu <silviu_miron_90@outlook.com>
2024-08-04 18:41:47 +0300

    Getting input and button reference

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Getting the reference to the input field and button
+
+const primaryName = document.querySelector('.first-name');
+const secondaryName = document.querySelector('.second-name');
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const btn = document.querySelector('.claim-button');
