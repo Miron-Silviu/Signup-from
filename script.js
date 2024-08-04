b236437a6048a02bc11ae17bee8f1697cded45f8
Silviu <silviu_miron_90@outlook.com>
2024-08-04 20:12:31 +0300

    Create function to check if the email is validate.

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ const secondaryName = document.querySelector('.second-name');
 const email = document.querySelector('.email');
 const password = document.querySelector('.password');
 const btn = document.querySelector('.claim-button');
+
+// Create function to validate email using a regular expression
+
+const isEmailValid = function (input) {
+  const emailPatern = /^[^\s@][+@[^\s@]+\.[^\s@]+$/;
+  return emailPatern.test(input);
+};
+
+// Create function to chck if the email is valid
+
+const isEmailInvalid = function (input) {
+  return !isEmailValid(input);
+};
+
+btn.addEventListener('click', function () {});
